2087bee5effb7575cf777dfae549f03c83bc9705
joao <dusiromulo@gmail.com>
2019-10-12 15:13:38 -0300

    prettier

--- App.js
+++ App.js
@@ -6,47 +6,51 @@
  * @flow
  */
 
-import React from 'react';
-import {useGlobal} from 'reactn';
+import React, {useGlobal} from 'reactn';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  Button,
-  Text,
-  StatusBar,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    Button,
+    Text,
+    StatusBar,
 } from 'react-native';
+import AppToolbar from './src/components/AppToolbar';
 
-const App: () => React$Node = () => {
-  const [value, setValue] = useGlobal('value');
+const App = () => {
+    const [value, setValue] = useGlobal('value');
 
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Text style={styles.footer}>{value}</Text>
-          <Button onPress={() => setValue(1)} title={'Change'} />
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <AppToolbar title={''} />
+            <SafeAreaView>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={styles.scrollView}>
+                    <Text style={styles.footer}>{value}</Text>
+                    <Button
+                        onPress={() => setValue(value + 1)}
+                        title={'Change'}
+                    />
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#FEFEFE',
-  },
-  footer: {
-    color: '#000000',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    scrollView: {
+        backgroundColor: '#FEFEFE',
+    },
+    footer: {
+        color: '#000000',
+        fontSize: 12,
+        fontWeight: '600',
+        padding: 4,
+        paddingRight: 12,
+        textAlign: 'right',
+    },
 });
 
 export default App;
